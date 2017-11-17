2933620b022825712ede033ee97c1297912abfc0
Andree Surya <andree.surya@gmail.com>
2017-11-17 15:14:54 +0700

    Skeleton key finder fragment view.

--- app/src/main/java/com/bukalapak/keyfinder/KeyFinderFragment.kt
+++ app/src/main/java/com/bukalapak/keyfinder/KeyFinderFragment.kt
@@ -1,10 +1,12 @@
 package com.bukalapak.keyfinder
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bukalapak.keyfinder.databinding.FragmentKeyFinderBinding
 
 /**
  * Created on : November/17/2017
@@ -14,9 +16,11 @@ import android.view.ViewGroup
  */
 class KeyFinderFragment : Fragment() {
 
+    private lateinit var binding: FragmentKeyFinderBinding
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_key_finder, container, false)
+
+        return binding.root
     }
-
-
 }
