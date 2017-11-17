0fb73887fa18fb04688b520cddc9884715200814
Andree Surya <andree.surya@gmail.com>
2017-11-17 17:27:08 +0700

    Move Beacon manager to KeyFinderFragment.

--- app/src/main/java/com/bukalapak/keyfinder/KeyFinderFragment.kt
+++ app/src/main/java/com/bukalapak/keyfinder/KeyFinderFragment.kt
@@ -1,12 +1,23 @@
 package com.bukalapak.keyfinder
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bukalapak.keyfinder.databinding.FragmentKeyFinderBinding
+import org.altbeacon.beacon.BeaconConsumer
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.Region
 
 /**
  * Created on : November/17/2017
@@ -14,15 +25,62 @@ import com.bukalapak.keyfinder.databinding.FragmentKeyFinderBinding
  * Company    : Bukalapak
  * Project    : KeyFinder
  */
-class KeyFinderFragment : Fragment() {
+class KeyFinderFragment : Fragment(), BeaconConsumer {
 
     private lateinit var binding: FragmentKeyFinderBinding
+    private lateinit var beaconManager: BeaconManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestLocationPermissions()
+
+        beaconManager = BeaconManager.getInstanceForApplication(context);
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BEACON_LAYOUT))
+        beaconManager.bind(this)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_key_finder, container, false)
 
-        
-
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        beaconManager.unbind(this)
+    }
+
+    private fun requestLocationPermissions() {
+
+        val permissions = arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        ActivityCompat.requestPermissions(activity, permissions, REQUEST_LOCATION_PERMISSION_CODE)
+    }
+
+    override fun getApplicationContext(): Context {
+        return activity.applicationContext
+    }
+
+    override fun unbindService(connection: ServiceConnection?) {
+        activity.unbindService(connection)
+    }
+
+    override fun bindService(service: Intent?, connection: ServiceConnection?, flags: Int): Boolean {
+        return activity.bindService(service, connection, flags)
+    }
+
+    override fun onBeaconServiceConnect() {
+        // Do something with beacon
+    }
+
+    companion object {
+        val TAG = KeyFinderFragment::class.java.simpleName
+
+        val REQUEST_LOCATION_PERMISSION_CODE = 1
+        val BEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
     }
 }
--- app/src/main/java/com/bukalapak/keyfinder/MainActivity.kt
+++ app/src/main/java/com/bukalapak/keyfinder/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.bukalapak.keyfinder
 
-import android.Manifest
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import org.altbeacon.beacon.BeaconConsumer
-import org.altbeacon.beacon.BeaconManager
-import org.altbeacon.beacon.BeaconParser
-import org.altbeacon.beacon.Region
 
-class MainActivity : AppCompatActivity(), BeaconConsumer{
+class MainActivity : AppCompatActivity() {
 
-    var beaconManager : BeaconManager? = null
-    val PERMISSION_REQUEST_COARSE_LOCATION = 1
+
     var pagerAdapter : PagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,46 +18,9 @@ class MainActivity : AppCompatActivity(), BeaconConsumer{
         pagerAdapter = PagerAdapter(supportFragmentManager)
         viewContainer.adapter = pagerAdapter
         createTabIcons()
-
-        ActivityCompat.requestPermissions(this,
-                arrayOf( Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSION_REQUEST_COARSE_LOCATION)
-
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        beaconManager?.beaconParsers?.add(BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
-        beaconManager?.bind(this)
     }
 
     private fun createTabIcons() {
-
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        beaconManager?.unbind(this)
-    }
-
-    override fun onBeaconServiceConnect() {
-
-        beaconManager?.addRangeNotifier({ beacons, _ ->
-            if (beacons.isNotEmpty()) {
-                Log.i(TAG, "The first beacon I see is about " + beacons.iterator().next().distance + " meters away.")
-                beaconManager?.stopRangingBeaconsInRegion(DIGIBAL)
-            }
-        })
-
-        try {
-            beaconManager?.startRangingBeaconsInRegion(DIGIBAL)
-        } catch (e: Throwable) {
-            Log.e(TAG, e.toString())
-        }
 
     }
 
@@ -85,12 +40,4 @@ class MainActivity : AppCompatActivity(), BeaconConsumer{
         }
 
     }
-
-    companion object {
-        val TAG = "MainActivity"
-        val DIGIBAL = Region("00001803-494C-4F47-4943-544543480000", null, null, null)
-    }
-
-
-
 }
