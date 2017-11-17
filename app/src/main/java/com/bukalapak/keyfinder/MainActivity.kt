664d7d1d83db7cbf42cdf0d63ba5b1853f3d5861
M Nafian Wildana <mnafian@icloud.com>
2017-11-17 17:57:54 +0700

    fix conflict

--- app/src/main/java/com/bukalapak/keyfinder/MainActivity.kt
+++ app/src/main/java/com/bukalapak/keyfinder/MainActivity.kt
@@ -1,31 +1,46 @@
 package com.bukalapak.keyfinder
 
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
-    var pagerAdapter : PagerAdapter? = null
+    lateinit var pagerAdapter : PagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        pagerAdapter = PagerAdapter(supportFragmentManager)
+        pagerAdapter = PagerAdapter(supportFragmentManager, this)
         viewContainer.adapter = pagerAdapter
-        createTabIcons()
+
+        tabView.setupWithViewPager(viewContainer)
+        for (i in 0 until tabView.tabCount) {
+            val tab = tabView.getTabAt(i)
+            tab?.customView = pagerAdapter.getTabView(i)
+        }
     }
 
-    private fun createTabIcons() {
 
-    }
-
-    class PagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
+    class PagerAdapter : FragmentPagerAdapter {
         val NUM_ITEMS = 2
+        var context : Context
+        var dataTabTitle = mutableListOf("KeyFinder", "Setting")
+        var dataTabImage = mutableListOf(R.drawable.ic_arrow, R.drawable.ic_tools)
+
+        constructor(fm: FragmentManager?, context : Context) : super(fm) {
+            this.context = context
+        }
+
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
@@ -37,6 +52,17 @@ class MainActivity : AppCompatActivity() {
 
         override fun getCount(): Int {
             return NUM_ITEMS
+        }
+
+        fun getTabView(position : Int) : View {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_tab_layout, null)
+            var imageView = view.findViewById<ImageView>(R.id.imageIcon)
+            var textView = view.findViewById<TextView>(R.id.textTitle)
+
+            textView.text = dataTabTitle[position]
+            imageView.setImageDrawable(ActivityCompat.getDrawable(context, dataTabImage[position]))
+
+            return view
         }
 
     }
